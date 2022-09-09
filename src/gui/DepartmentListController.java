f9158ac0be1a9ceae89c7dacbe99d5ec9d12b837
josuejhoseff <josuejhosefff@gmail.com>
2022-09-09 10:41:45 -0300

    Department Service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,13 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	
+	
+	private DepartmentService service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartment;
 	@FXML
@@ -23,6 +31,12 @@ public class DepartmentListController implements Initializable {
 	
 	@FXML
 	private Button btNew;
+	
+	private ObservableList<Department> obsList;
+	
+	public void setDepartmentService(DepartmentService Service) {//injetar dependencia
+		this.service = Service;
+	}
 	
 	@FXML
 	public void onBtNewAction() {
@@ -35,11 +49,21 @@ public class DepartmentListController implements Initializable {
 		
 	}
 	private void inititalizeNodes() {
-		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("ID")); //inicializar a coluna para não ter erro
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id")); //inicializar a coluna para não ter erro
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("Name"));//inicializar a coluna para não ter erro
 		
 		Stage stage =(Stage) Main.getMainScene().getWindow(); // fazer com que ...
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());// a tabela fique no mesmo tamanho da tela
+	}
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Table was null");
+		}
+		List<Department>list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+		
 	}
 
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -32,7 +33,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 
 	@FXML
@@ -59,6 +60,28 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);// adiciono a barra de menu novamente e
 			mainVBox.getChildren().addAll(newVBox);// adiciono a nova cena que sera exibida
 			
+			
+		}
+		catch(IOException e) {
+			Alerts.showAlerts("IO Exception","Erro loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	private synchronized void loadView2(String absoluteName) { //synchronized para este elemento rodar sem interrupção
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent(); //getRoot() pega o primeiro elemento
+			
+			Node mainMenu = mainVBox.getChildren().get(0);//get(0) é a posição do elemento (primeiro filho) neste caso a barra de menu
+			mainVBox.getChildren().clear(); //limpo tudo
+			mainVBox.getChildren().add(mainMenu);// adiciono a barra de menu novamente e
+			mainVBox.getChildren().addAll(newVBox);// adiciono a nova cena que sera exibida
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
 			
 		}
 		catch(IOException e) {
