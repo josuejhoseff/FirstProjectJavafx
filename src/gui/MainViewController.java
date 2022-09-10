fc827e8b19c185e3d7487ee3ce0e6baaca9245f8
josuejhoseff <josuejhosefff@gmail.com>
2022-09-10 10:02:48 -0300

    Inicialização por paramentro (lambda/consumer)

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -33,12 +34,15 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) ->{
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
 	public void onMenuAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 	@Override
@@ -47,7 +51,7 @@ public class MainViewController implements Initializable {
 
 	}
 
-	private synchronized void loadView(String absoluteName) { //synchronized para este elemento rodar sem interrupção
+	private synchronized <T>void loadView(String absoluteName, Consumer<T> initilializeAction) { //synchronized para este elemento rodar sem interrupção
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
@@ -60,28 +64,9 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);// adiciono a barra de menu novamente e
 			mainVBox.getChildren().addAll(newVBox);// adiciono a nova cena que sera exibida
 			
+			T controller = loader.getController();
+			initilializeAction.accept(controller);
 			
-		}
-		catch(IOException e) {
-			Alerts.showAlerts("IO Exception","Erro loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	private synchronized void loadView2(String absoluteName) { //synchronized para este elemento rodar sem interrupção
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent(); //getRoot() pega o primeiro elemento
-			
-			Node mainMenu = mainVBox.getChildren().get(0);//get(0) é a posição do elemento (primeiro filho) neste caso a barra de menu
-			mainVBox.getChildren().clear(); //limpo tudo
-			mainVBox.getChildren().add(mainMenu);// adiciono a barra de menu novamente e
-			mainVBox.getChildren().addAll(newVBox);// adiciono a nova cena que sera exibida
-			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
 			
 		}
 		catch(IOException e) {
