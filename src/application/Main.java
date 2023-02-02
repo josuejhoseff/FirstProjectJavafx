d8a46d8c92c298e8ad8321fe2bf2afaefe54b045
Josué Jhoseff <josuejhosefff@gmail.com>
2023-02-02 17:03:25 -0300

    readme

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,20 +11,17 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	
 	private static Scene mainScene;
-	public static Scene getMainScene() {
-		return mainScene;
-	}
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			ScrollPane scrollpane = loader.load();
+			ScrollPane scrollPane = loader.load();
 			
-			scrollpane.setFitToHeight(true); //Ajustar a tela
-			scrollpane.setFitToWidth(true); // Ajustar a tela
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
 			
-			mainScene = new Scene(scrollpane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -33,6 +30,10 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
